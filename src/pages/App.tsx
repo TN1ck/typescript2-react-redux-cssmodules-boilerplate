192adf7a6c66ba75e0a51a6d8c7e7ed063304c04
TN1ck <tomwanick@gmail.com>
2016-10-15 18:49:06 +0200

    init

---
+++ src/pages/App.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import * as Grid from 'src/components/Grid';
+import * as Navigation from 'src/components/Navigation';
+
+interface IAppProps extends React.Props<any> {
+};
+
+class App extends React.Component<IAppProps, void> {
+    render () {
+        return (
+            <div>
+                <Grid.Container>
+                    <Grid.Row>
+                        <Navigation.Wrapper>
+                            <Navigation.Item to={'/home'}>{'Home'}</Navigation.Item>
+                            <Navigation.Item to={'/counter'}>{'Counter'}</Navigation.Item>
+                        </Navigation.Wrapper>
+                    </Grid.Row>
+                </Grid.Container>
+                {this.props.children}
+            </div>
+        );
+    }
+};
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import * as Grid from 'src/components/Grid';
+
+export default class Home extends React.Component<{}, {}> {
+    render () {
+        return (
+            <div>
+                <Grid.Container>
+                    <Grid.Row>
+                        <Grid.Col xs={12}>
+                            <h1>{'Introduction'}</h1>
+                            <p>
+                                {'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'}
+                            </p>
+                        </Grid.Col>
+                    </Grid.Row>
+                </Grid.Container>
+                {this.props.children}
+            </div>
+        );
+    }
+};
